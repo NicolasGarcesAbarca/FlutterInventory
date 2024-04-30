591132a372d9a826156a3adb40e90d39675bd3dd
nicko <nickogarces@gmail.com>
2024-04-30 16:53:10 -0400

    flutter boilerplate init

---
+++ android/app/src/main/kotlin/com/inventory/inventory/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.inventory.inventory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Inventario',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          useMaterial3: true,
+        ),
+        home: DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              appBar: AppBar(
+                bottom: const TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.directions_car)),
+                    Tab(icon: Icon(Icons.directions_transit)),
+                    Tab(icon: Icon(Icons.directions_bike)),
+                  ],
+                ),
+              ),
+              body: const TabBarView(children: [
+                Icon(Icons.ac_unit_rounded),
+                Icon(Icons.access_alarm_rounded),
+                Icon(Icons.ad_units_sharp)
+              ]),
+            )) //const MyHomePage(title: 'Flutter Demo Home Page'),
+        );
+  }
+}
